a0d6be76dcd42ad968d63888b745dcabe090e3e6
Degisew <jegisew21@gmail.com>
2022-10-17 01:07:09 +0300

    Created a skills page

---
+++ src/Pages/Skills.js
@@ -0,0 +1,13 @@
+import React from "react";
+import sharedSelectors from "./SharedStyles.module.css";
+const Skills = () => {
+  return (
+    <section>
+      <div className={sharedSelectors.title}>
+        <h1>SKILLS</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
