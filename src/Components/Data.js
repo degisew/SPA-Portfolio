60c83f3ee0dbea2c6833d0c1070066f8a7ba4d26
Degisew <jegisew21@gmail.com>
2022-10-17 23:59:28 +0300

    Added a Data.js file

---
+++ src/Components/Data.js
@@ -0,0 +1,60 @@
+const skills = {
+  frontEnd: ["React", "Redux", "JavaScript", "HTML5", "CSS3", "SCSS"],
+  backEnd: ["Ruby", "Ruby on Rails"],
+  ToolsAndMethods: [
+    "Git",
+    "GitHub",
+    "Netlify",
+    "Mobile/Responsive development",
+    "RSpec",
+    "TDD",
+    "Chrome Dev Tools",
+  ],
+  professional: [
+    "Remote Pair programming",
+    "TeamWork",
+    "Mentoring",
+    "Communication Skills",
+  ],
+};
+
+const experience = [
+    {
+        id: 1,
+        company: 'Bahir Dar University',
+        role: 'Assistant',
+        duration: 'Oct, 2021 - present',
+    },
+    {
+        id: 2,
+        company: 'Microverse',
+        role: 'Mentor',
+        duration: 'June, 2022 - present',
+    },
+];
+
+const projects = [
+    {
+        id:1,
+        title: 'Meetup',
+        builtWith: ['React','CSS3', 'Firebase'],
+        description: '',
+    },
+    {
+        id:2,
+        title: 'Meal App',
+        builtWith: ['HTML5','CSS3','JavaScript','TheMealDBAPI'],
+        description: '',
+    },
+    {
+        id:1,
+        title: 'Math Magicians',
+        builtWith: ['React','SCSS'],
+        description: '',
+    }
+]
+
+const contact = {
+    address: 'Bahir Dar Ethiopia',
+    Phone: '+251953059021'
+}
