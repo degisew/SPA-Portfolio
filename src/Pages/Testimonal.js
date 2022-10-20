1de62da219b93ff9a9b7837621da5e47c6331760
Degisew <jegisew21@gmail.com>
2022-10-20 23:53:31 +0300

    Created a file

---
+++ src/Pages/Testimonal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import sharedSelectors from '../Pages/SharedStyles.module.css';
+
+const Testimonal = () => {
+  return (
+    <section>
+      <div className={sharedSelectors.title}>
+        <h1>Client Testimonals</h1>
+      </div>
+    </section>
+  );
+}
+
+export default Testimonal
