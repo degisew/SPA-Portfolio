19056ae7f11f3474a073f6b5b4305f27cf9d0527
Degisew <jegisew21@gmail.com>
2022-10-21 18:08:33 +0300

    created a testimonal page

--- src/Pages/Testimonal.js
+++ src/Pages/Testimonal.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import sharedSelectors from '../Pages/SharedStyles.module.css';
-
+import { testimonals } from '../Components/Data';
 const Testimonal = () => {
   return (
     <section>
       <div className={sharedSelectors.title}>
-        <h1>Client Testimonals</h1>
+        <h1> TESTIMONAL</h1>
+      </div>
+      <div>
+        {testimonals ? <p>true</p> : <p>Nothing Yet!</p>}
       </div>
     </section>
   );
